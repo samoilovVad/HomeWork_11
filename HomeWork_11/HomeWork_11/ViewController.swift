fc41fa72870701706d6e77cb0a5a966122bce306
Vadim <firstkoks@gmail.com>
2022-11-12 14:28:58 +0300

    create new project

---
+++ HomeWork_11/HomeWork_11/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork_11
+//
+//  Created by Vadim Samoilov on 12.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
