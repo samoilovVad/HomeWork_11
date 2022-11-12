40eb4f06758613b5d635315fa5641b81026d901d
Vadim <firstkoks@gmail.com>
2022-11-12 15:32:16 +0300

    add button for unwind segue

--- HomeWork_11/HomeWork_11/FirstViewController.swift
+++ HomeWork_11/HomeWork_11/FirstViewController.swift
@@ -8,17 +8,22 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
-    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var textLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "unwindSegue" {
+            guard let destination = segue.destination as? SecondViewController else {return}
+            destination.text = "from button unwind segue"
+        }
         guard segue.identifier == "showSecondViewController" else {return}
         guard let destination = segue.destination as? SecondViewController else {return}
         destination.name = "John"
+        
     }
     
     
@@ -27,6 +32,14 @@ class FirstViewController: UIViewController {
         guard let second = storyboard.instantiateViewController(withIdentifier: "SecondViewController") as? SecondViewController else {return}
         second.name = "Paul"
         show(second, sender: nil)
+    }
+    
+    @IBAction func saveData(_ unwindSegue: UIStoryboardSegue) {
+        guard unwindSegue.identifier == "passDataToFirstVC" else {
+            return
+        }
+        guard let source = unwindSegue.source as? SecondViewController else { return }
+        textLabel.text = source.text
     }
     
 }
--- HomeWork_11/HomeWork_11/SecondViewController.swift
+++ HomeWork_11/HomeWork_11/SecondViewController.swift
@@ -10,12 +10,19 @@ import UIKit
 class SecondViewController: UIViewController {
     
     var name = ""
+    var text = ""
 
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var textLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         nameLabel.text = name
+        textLabel.text = text
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        text = "Data was passed"
     }
 
 }
